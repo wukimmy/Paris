4b233b85451a8dcf409119f065c974bf7694ca0d
nathalya-katheryne <nathalya_katheryne@hotmail.com>
2018-05-15 13:03:37 -0300

    Inicio do desenvolvimento do index

---
+++ static/js/personalizacao.js
@@ -0,0 +1,9 @@
+$('.carousel.carousel-slider').carousel({
+	fullWidth: true
+});
+
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.carousel').carousel();
+    $('.parallax').parallax();
+});
