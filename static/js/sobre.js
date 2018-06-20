aa39d33271f1a2994b036e08c29b7d94787397f8
nathalya <nathalyakatheryne@gmail.com>
2018-06-20 02:53:06 -0300

    desenvolvimento das páginas

---
+++ static/js/sobre.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.parallax').parallax();
+    $(".dropdown-trigger").dropdown();
+
+    $(window).scroll(function(){
+        if($(window).scrollTop() > 0) {
+            $('nav').css('background-color', 'black');
+        } else {
+            $('nav').css('background-color', 'rgba(0, 0, 0, 0.3)');
+        }
+    });
+
+    if($(window).scrollTop() > 0) {
+        $('nav').css('background-color', 'black');
+    } else {
+        $('nav').css('background-color', 'rgba(0, 0, 0, 0.3)');
+    }
+});
+
+$(document).ready(function(){
+    $('.carousel').carousel();
+});
+
+
+
